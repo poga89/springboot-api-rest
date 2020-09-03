b4ab9e0821d7dff8e071754f3801a4f2a1fdeb4a
Polo <poga89@hotmail.com>
2020-09-02 22:18:11 -0500

    nuevo api para facuturas

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Factura;
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Region;
 
 public interface IClienteService {
@@ -22,4 +23,10 @@ public interface IClienteService {
 	
 	public List<Region> findAllRegiones();
 
+	public Factura findFacturaById(Long id);
+	
+	public Factura save(Factura factura);
+	
+	public void deleteFacturaById(Long id);
+	
 }
